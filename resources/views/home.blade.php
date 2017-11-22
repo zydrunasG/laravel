157c26c1f73ef8bd8d5b9df38b992c29a235bb38
rakasx2 <a.rakovskis@gmail.com>
2017-11-22 15:36:16 +0200

    Aiksteliu skaicius

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,7 @@
 @extends('base')
 
 @section('content')
-    <h1 class="page-header text-center">Naujausios 10 aikštelių</h1>
+    <h1 class="page-header text-center">Naujausios 20 aikštelių</h1>
     @foreach($newest_courts as $court)
         <a href="{{ url('aikstele/'. $court->id) }}"><h4>{{ $court->title }}</h4></a>
         {{ $court->address }}, {{ $court->city->title }}
