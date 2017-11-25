99d8cb45e9549cb1163e76a98c2b1b6b68e9cee6
ZydrunasG <zydrunas.gaidukovas@gmail.com>
2017-11-25 22:33:46 +0200

    commit

--- resources/views/auth/passwords/password.blade.php
+++
@@ -1,2 +0,0 @@
-To reset click link <br>
-<a href="{{ $link = url('password/reset', $token).'?email='.urlencode($user->getEmailForPasswordReset()) }}">{{ $link }}</a>
---
+++ resources/views/vendor/notifications/email.blade.php
@@ -0,0 +1,1 @@
+<a href="{{ $link = url('password/reset', $token).'?email='.urlencode($user->getEmailForPasswordReset()) }}">{{ $link }}</a>
