3d97e005215e2e852ca3b6e3c605618f84e3bbc7
ZydrunasG <zydrunas.gaidukovas@gmail.com>
2017-12-18 19:49:07 +0200

    updated migrations file

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email');
             $table->string('password');
             $table->string('verifyToken');
-            $table->boolean('status');
+            $table->boolean('status')->default('0');
             $table->rememberToken();
             $table->timestamps();
         });
